97974fa7daff3c7248d4cfb56fd3201bfe8a057b
eonz1 <meonzzz1@gmail.com>
2023-12-03 19:16:29 +0900

    회원가입 api, retrofit 으로 변경

---
+++ lib/feature/sign_up/api/sign_up_repository.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:photo_card_flutter/feature/sign_up/api/sign_up_request.dart';
+import 'package:photo_card_flutter/feature/sign_up/api/verify_email_request.dart';
+import 'package:retrofit/http.dart';
+
+import '../../../global/api/response_entity.dart';
+
+part 'sign_up_repository.g.dart';
+
+@RestApi()
+abstract class SignUpRepository {
+  factory SignUpRepository(Dio dio, {String baseUrl}) = _SignUpRepository;
+
+  @POST("/member")
+  Future<void> signUp(
+    @Body() SignUpRequest signUpRequest,
+  );
+
+  @GET("/user-id/check")
+  Future<ResponseEntity<String>> validId(@Query('id') String id);
+
+  @GET("/auth/email")
+  Future<ResponseEntity<String>> getEmailCode(@Query('email') String email);
+
+  @POST("/auth/email")
+  Future<ResponseEntity<String>> verifyEmailCode(
+      @Body() VerifyEmailRequest verifyEmailRequest);
+}
