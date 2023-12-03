2fa548ec34c07fe489c4bf093140e508fd8ac11f
eonz1 <meonzzz1@gmail.com>
2023-12-03 19:02:19 +0900

    개발 api baseurl 추가

--- lib/global/api/api_route_constants.dart
+++ lib/global/api/api_route_constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 class ApiRouteConstants {
   static String getBaseUrl() {
     if (kDebugMode) {
-      return "http://photocard.site:8080";
+      return "https://0f4f-203-132-166-14.ngrok-free.app/photocard";
     } else {
       // production mode
       return "http://photocard.site:8080";
