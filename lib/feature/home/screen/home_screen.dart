f7dc0e1e9e6b6f6efa2dc6c4bd944ee7a1000946
eonz1 <meonzzz1@gmail.com>
2023-12-06 21:17:21 +0900

    홈화면에 회원가입화면 제거

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../bottom_navigation/screen/custom_bottom_navigation_bar.dart';
-import '../../sign_up/screen/sign_up_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,21 +14,6 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(40)),
-              onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SignUpScreen(),
-                    ),
-                  ),
-              child: const Text("가입하기")),
-        ],
-      ),
       bottomNavigationBar: const CustomBottomNavigationBar(),
     ));
   }
